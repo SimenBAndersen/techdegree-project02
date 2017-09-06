f1242d7c900a3e1d30a642cce918dd1e46107a90
Simen Bjerkeset Andersen <simen-bandersen@hotmail.no>
2017-09-06 11:00:06 +0200

    added a first draft of the showPage function in script.js
    Shows 10 students based on a page number and a list of students

--- pagination-filter-v2/pagination-filter-v2/script.js
+++ pagination-filter-v2/pagination-filter-v2/script.js
@@ -1,1 +1,28 @@
-console.log("Script is loaded..")
+// On load function
+$(function() {
+  // All the students
+  const $students = $(".student-item");
+
+  // Display page and list of students
+  function showPage(pageNumber, studentList) {
+
+    // Hide the students on the page
+    studentList.hide();
+
+    // Find the index of the students to show
+    let $studentIndex = (pageNumber - 1) * 10;
+
+    // Select and show 10 students, starting from $studentIndex
+    studentList.slice($studentIndex, $studentIndex + 10).show();
+
+  }
+
+  // Create page links
+  // Call showPage() to display list of students
+  function appendPageLinks(studentList) {
+    
+  }
+
+  // TEST:
+  // showPage(1, $students);
+});
